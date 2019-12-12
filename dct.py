d5995e1b5e819e20a030c5a3ac0d8fc8e74a3fb9
HarshalGoyal <53938512+HarshalGoyal@users.noreply.github.com>
2019-12-12 19:29:03 +0530

    Add files via upload

---
+++ dct.py
@@ -0,0 +1,35 @@
+import wikipedia
+import pyttsx3
+import speech_recognition as sr
+
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Listening")
+    r.pause_threshold = 2
+    audio = r.listen(source)
+
+    try:
+        print("Recognising")
+        qurey = r.recognize_google(audio, language='en-in')
+        print(f"user input:{qurey}\n ")
+        wikipedia.search(r.recognize_google(audio, language='en-in'))
+
+        engine = pyttsx3.init()
+
+        res=wikipedia.summary(r.recognize_google(audio, language='en-in'))
+
+        print("According to wikipedia : %s"%res)
+
+        engine.say(res)
+        engine.runAndWait()
+
+    except Exception as e:
+
+        print("please re Run the program")
+        
+
+
+
+
+
